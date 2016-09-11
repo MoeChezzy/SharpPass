751c4479f079b715c87bba46f82c8a63959b8ec1
Chezzy <chezrye@gmail.com>
2016-09-10 23:10:12 -0400

    Renamed HashHelper class to SecurityHelper

--- SharpPass/SecurityHelper.cs
+++ SharpPass/SecurityHelper.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Text;
+using System.Security;
 using System.Security.Cryptography;
 
 namespace SharpPass
 {
-    public class HashHelper
+    public class SecurityHelper
     {
         private const int SaltByteSize = 64; // Default: 24
         private const int HashByteSize = 256; // Default: 20
@@ -37,6 +38,25 @@ namespace SharpPass
         /// <returns>Returns whether the string is valid.</returns>
         public static bool Validate(string input, string hash)
         {
+            string[] splitHash = hash.Split(Delimiter);
+            int iterations = int.Parse(Encoding.UTF8.GetString(Convert.FromBase64String(splitHash[IterationIndex])));
+            byte[] saltBytes = Convert.FromBase64String(splitHash[SaltIndex]);
+            byte[] hashBytes = Convert.FromBase64String(splitHash[PBKDF2Index]);
+
+            byte[] testHash = GetPBKDF2Bytes(input, saltBytes, iterations, hashBytes.Length);
+            return Check(hashBytes, testHash);
+        }
+
+        /// <summary>
+        /// Checks whether a given string matches a hash.
+        /// </summary>
+        /// <param name="inputSecureString">The SecureString of the text to check.</param>
+        /// <param name="hashSecureString">The SecureString of the hash to validate against.</param>
+        /// <returns>Returns whether the string is valid.</returns>
+        public static bool Validate(SecureString inputSecureString, SecureString hashSecureString)
+        {
+            string input = CredentialSet.ConvertToString(inputSecureString);
+            string hash = CredentialSet.ConvertToString(hashSecureString);
             string[] splitHash = hash.Split(Delimiter);
             int iterations = int.Parse(Encoding.UTF8.GetString(Convert.FromBase64String(splitHash[IterationIndex])));
             byte[] saltBytes = Convert.FromBase64String(splitHash[SaltIndex]);
